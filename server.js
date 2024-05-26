250bf7ec85c68304c1b985891b1f7e1e7439759e
AnkithSanga <ankithsanga1234@gmail.com>
2024-05-27 00:09:36 +0530

    mostly done, deployment and .env variable usage left.

--- server.js
+++ server.js
@@ -9,3 +9,20 @@ require('dotenv').config
 const {GoogleGenerativeAI} = require('@google/generative-ai')
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY)
+
+app.post('/gemini',async (req,res)=>{
+
+        const model = genAI.getGenerativeModel({model:"gemini-pro"});
+
+        const chat = model.startChat({
+            history:req.body.history
+        })
+        const msg = req.body.message;
+
+        const result = await chat.sendMessage(msg);
+        const response = await result.response;
+        const text = response.text()
+        res.send(text);
+})
+
+app.listen(PORT,()=> console.log(`Listening on port ${PORT}`))
--- src/App.js
+++ src/App.js
@@ -12,58 +12,82 @@ const App = () => {
     "What is the best thing about India?",
   ];
 
-const surprise = () => {
-  const randomIndex = Math.floor(Math.random() * surpriseOptions.length);
-  setValue(surpriseOptions[randomIndex]);
-  setError("");
-}
+  const surprise = () => {
+    const randomIndex = Math.floor(Math.random() * surpriseOptions.length);
+    setValue(surpriseOptions[randomIndex]);
+  };
 
-const getResponse = async () =>{
-    if(!value){
+  const getResponse = async () => {
+    if (!value) {
       setError("Go on! Throw a question mate");
       return;
     }
-    try{
-        const options = {
-            method: 'POST',
-            body : JSON.stringify({
-              history:chatHistory,
-              message:value
-            }),
-          headers:{
-            'Content-Type': 'application/json'
-          }
-        }
-        const response = await fetch("https://localhost:8000/gemini",options);
-        const data  = await response.text();
-        console.log(data);
-    }
-    catch(error){
+    try {
+      const options = {
+        method: "POST",
+        body: JSON.stringify({
+          history: chatHistory,
+          message: value,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      const response = await fetch("http://localhost:8000/gemini", options);
+      const data = await response.text();
+      setChatHistory((oldChatHistory) => [
+        ...oldChatHistory,
+        {
+          role: "user",
+          parts: value,
+        },
+        {
+          role: "model",
+          parts: data,
+        },
+      ]);
+      setValue("");
+    } catch (error) {
       console.error(error);
       setError("Something went wrong mate, try again");
     }
-}
+  };
+
+  const clear = () =>{
+    setChatHistory([]);
+    setValue("");
+    setError("");
+  }
 
   return (
-
-      <section className="app">
-        <p>
-          What's up user? What information do you want to seek?
-          <button className="surprise" onClick={surprise} disabled={!chatHistory}> Surprise!</button>
-        </p>
-        <div className="input-container">
-          <input value={value} placeholder="when is diwali?" onChange={(e) => setValue(e.target.value)} />
-          {!error && <button>Ask me</button>}
-          {error && <button>Clear me</button>}
-        </div>
-        {error && <p>{error}</p>}
-        <div className="result-container">
-          <div key={""}>
-            <p className="answer"></p>
+    <section className="app">
+      <p>
+        What's up user? What information do you want to seek?
+        <button className="surprise" onClick={surprise} disabled={!chatHistory}>
+          {" "}
+          Surprise!
+        </button>
+      </p>
+      <div className="input-container">
+        <input
+          value={value}
+          placeholder="when is diwali?"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        {!error && <button onClick={getResponse}>Ask me</button>}
+        {error && <button onClick={clear}>Clear me</button>}
+      </div>
+      {error && <p>{error}</p>}
+      <div className="result-container">
+        {chatHistory.map((chatItem, index) => (
+          <div key={index}>
+            <p className="answer">
+              {chatItem.role} : {chatItem.parts}
+            </p>
           </div>
-        </div>
-      </section>
-
+        ))}
+      </div>
+    </section>
   );
 };
 
