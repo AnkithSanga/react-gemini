be085e1ac11f93f18c38a5a1357353f404389206
AnkithSanga <ankithsanga1234@gmail.com>
2024-05-26 15:55:36 +0530

    added few files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { useState } from "react";
+import "./App.css";
 
-const  App = () => {
+const App = () => {
+  const [error, setError] = useState("");
   return (
-    <div className="app">
-      <section>
-    </div>
+
+      <section className="app">
+        <p>
+          What's up user? What information do you want to seek?
+          <button className="surprise"> Surprise!</button>
+        </p>
+        <div className="input-container">
+          <input value={""} placeholder="when is diwali?" onChange={""} />
+          {!error && <button>Ask me</button>}
+          {error && <button>Clear me</button>}
+        </div>
+        {error && <p>{error}</p>}
+        <div className="result-container">
+          <div key={""}>
+            <p className="answer"></p>
+          </div>
+        </div>
+      </section>
+
   );
-}
+};
 
 export default App;
